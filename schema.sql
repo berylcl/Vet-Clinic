b7d4f3bcc38c62a481d5b1ecec7ab096252c9589
berylcl <gakiiberyl@gmail.com>
2023-07-12 09:13:44 +0100

    Add schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar(100),
+    date_of_birth  DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL(10,2) NOT NULL
 );
