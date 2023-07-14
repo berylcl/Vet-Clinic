2d43de1d77e1b2c053ee7c820cb7bed519d0bf87
berylcl <gakiiberyl@gmail.com>
2023-07-14 11:17:02 +0100

    Include queries for analyzing data and answering specific questions

--- queries.sql
+++ queries.sql
@@ -147,12 +147,13 @@ ORDER BY v.visit_date ASC
 LIMIT 1;
 
 -- Query 7: Details for the most recent visit: animal information, vet information, and date of visit.
-SELECT a.name AS animal_name, v.name AS vet_name, v.visit_date
+SELECT a.name AS animal_name, ve.name AS vet_name, v.visit_date
 FROM animals AS a
 JOIN visits AS v ON a.id = v.animal_id
 JOIN vets AS ve ON v.vet_id = ve.id
 WHERE v.visit_date = (SELECT MAX(visit_date) FROM visits)
 LIMIT 1;
+
 
 -- Query 8: How many visits were with a vet that did not specialize in that animal's species?
 SELECT COUNT(*) AS mismatched_visits
@@ -163,13 +164,12 @@ LEFT JOIN specializations AS sp ON ve.id = sp.vet_id AND a.species_id = sp.speci
 WHERE sp.vet_id IS NULL;
 
 -- Query 9: What specialty should Maisy Smith consider getting? Look for the species she gets the most.
-SELECT s.name AS specialty_name, COUNT(*) AS visit_count
-FROM animals AS a
-JOIN visits AS v ON a.id = v.animal_id
-JOIN vets AS ve ON v.vet_id = ve.id
-JOIN specializations AS sp ON ve.id = sp.vet_id
-JOIN species AS s ON sp.species_id = s.id
-WHERE ve.name = 'Maisy Smith'
+SELECT s.name AS specialty, COUNT(*) AS visit_count
+FROM visits v
+JOIN vets vt ON v.vet_id = vt.id
+JOIN animals a ON v.animal_id = a.id
+JOIN species s ON a.species_id = s.id
+WHERE vt.name = 'Maisy Smith'
 GROUP BY s.name
 ORDER BY visit_count DESC
 LIMIT 1;
